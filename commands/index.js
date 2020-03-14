881c56d64262ecf8052f06504894d02c67936d39
Aventhor <vaster399@gmail.com>
2020-03-14 21:32:44 +0300

    fixed run commands

--- commands/index.js
+++ commands/index.js
@@ -5,23 +5,11 @@ let sys = require('./sysinfo');
 let help = require('./help');
 const botUnsleep = require('./unsleep');
 
-exports.sys = () => {
-    sys.getInfo();
-}
-
-exports.dropMeme = (bot, channelID) => {
-    gooseMeme.drop(bot, channelID);
-}
-
-exports.replyMessage = (bot, message, channelID, msg) => {
-    replyMessage.reply(bot, message, channelID, msg);
-}
-
-exports.help = (bot, message) => {
-    help.help(bot, message);
-}
-
 module.exports = {
+    sys: () => sys.getInfo(),
+    help: (bot, message) => help.help(bot, message),
     sleep: (bot, message) => botSleep.sleep(bot, message),
     unsleep: (bot, message) => botUnsleep.unsleep(bot, message),
+    dropMeme: (bot, channelID) => gooseMeme.drop(bot, channelID),
+    replyMessage: (bot, message, channelID, msg) => replyMessage.reply(bot, message, channelID, msg),
 }
